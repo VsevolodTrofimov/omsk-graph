c19ab874d5abc815736cecf25897bdbca7c3c4b7
Vsevolod Trofimov <v-trof@yandex-team.ru>
2020-05-11 16:25:17 +0300

    Added shapefiles, removed not working code form 1.py

--- 1/1.py
+++ 1/1.py
@@ -12,7 +12,7 @@ with open('../getBuildings/filtered/infra.json', encoding='utf-8') as f:
 G = ox.save_load.load_graphml('omsk.graphml')
 
 # do pathfinding
-route = nx.shortest_path(G, apartments[0]['nodes'][0], infra[0]['nodes'][0])
-ox.plot_graph_route(G, route)
+# route = nx.shortest_path(G, apartments[0]['nodes'][0], infra[0]['nodes'][0])
+# ox.plot_graph_route(G, route)
 
 ox.plot_graph(G)
